24638a291e40bd75c945d77eef0d3370eb853ebf
michael casey <mike100casey@outlook.ie>
2016-03-10 15:30:43 +0000

    Added manifest

---
+++ src/main/java/Tester.java
@@ -0,0 +1,10 @@
+/***
+ * Created by Michael on 3/8/2016.
+ */
+public class Tester {
+    public static void main(String[] args) {
+        Customer customer1 = new Customer("mike",100);
+        String name = customer1.getName();
+        System.out.println(name);
+    }
+}
