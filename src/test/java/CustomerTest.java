8f541f28f78a63b50de4ed62cf93399693859249
michael casey <mike100casey@outlook.ie>
2016-03-15 15:19:16 +0000

    Added index file

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -20,6 +20,7 @@ public class CustomerTest {
     
     public CustomerTest() {
     }
+
     /**
      * Test of takeoutloan method, of class Customer.
      */
@@ -27,22 +28,13 @@ public class CustomerTest {
     public void testTakeoutloan() {
         Customer instance = new Customer("Jake Black",0.0);
        
-         /*call the createMock to create a mock for the Loan class */
         mockloan = EasyMock.createMock(Loan.class);
-     
-        /* set up the expected values and return values */
         expect(mockloan.getMonthlyPayment()).andReturn(106.23);
-        
-        /*activate the mock */
         replay(mockloan);
-       
         double expResult = 106.23;
-
         instance.takeoutloan();
         double result = instance.getMonthlypayments();
         assertEquals("The monthly payments must be equal", expResult, result, 0.5) ; 
-
-
     }
     
 }
